e8570a09fa792ad47f572481382b005a94b913f6
vinodekumar123 <tayyarihub1@gmail.com>
2025-09-09 21:48:26 +0500

    Create next-sitemap.config.js

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://tayyari-hub.vercel.app', // change to your live domain
+  generateRobotsTxt: true, // generate robots.txt
+  sitemapSize: 7000,       // optional
+}
