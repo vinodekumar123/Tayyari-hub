6d86c3304f60b4f77c1ff717d5a8e51dd6041f31
vinodekumar123 <tayyarihub1@gmail.com>
2025-09-09 21:11:15 +0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,43 +1,27 @@
-import './globals.css';
-import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
-import './globals.css';
-import { Analytics } from '@vercel/analytics/next';
- 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return (
-    <html lang="en">
-      <head>
-        <title>Next.js</title>
-      </head>
-      <body>
-        {children}
-        <Analytics />
-      </body>
-    </html>
-  );
-}
-const inter = Inter({ subsets: ['latin'] });
+// app/layout.tsx
+import './globals.css'
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+import { Analytics } from '@vercel/analytics/next'
+
+const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'Tayyari Hub',
   description: 'Best Platform for MDCAT, LAT and ECAT Entry Test Prepration',
-};
+}
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode;
+  children: React.ReactNode
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        {children}
+        <Analytics />
+      </body>
     </html>
-  );
+  )
 }
-// app/layout.tsx
-
