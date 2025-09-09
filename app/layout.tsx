ab0bd46a54a82cc292c6e2699354827519a4cd47
vinodekumar123 <tayyarihub1@gmail.com>
2025-09-09 20:59:15 +0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,25 @@ import './globals.css';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
-
+import { Analytics } from '@vercel/analytics/next';
+ 
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <head>
+        <title>Next.js</title>
+      </head>
+      <body>
+        {children}
+        <Analytics />
+      </body>
+    </html>
+  );
+}
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
