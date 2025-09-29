026c07e09af62cd16dbb086623a0ab1a9f7d658d
vinodekumar123 <tayyarihub1@gmail.com>
2025-09-29 23:50:02 +0500

    Update page.tsx

--- app/admin/quizzes/user-created-quizzes/page.tsx
+++ app/admin/quizzes/user-created-quizzes/page.tsx
@@ -4,7 +4,7 @@ import { useEffect, useState } from 'react';
 import { useRouter } from 'next/navigation';
 import { collection, getDocs, query, where, orderBy } from 'firebase/firestore';
 import { onAuthStateChanged, User } from 'firebase/auth';
-import { db, auth } from '../../firebase';
+import { db, auth } from 'app/firebase';
 import { Card, CardHeader, CardTitle, CardContent } from '@/components/ui/card';
 import { Button } from '@/components/ui/button';
 import { Badge } from '@/components/ui/badge';
